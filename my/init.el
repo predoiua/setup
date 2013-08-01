89a8f596fb60e82834d85ec1058163f2d2ecae5c
Seli <predoiu.seli@gmail.com>
2013-08-01 18:15:23 +0300

    My config.

---
+++ my/init.el
@@ -0,0 +1,159 @@
+;; -*- mode: emacs-lisp -*-
+;; Simple .emacs configuration
+
+;; ---------------------
+;; -- Global Settings --
+;; ---------------------
+(add-to-list 'load-path "~/.emacs.d")
+(require 'cl)
+(require 'ido)
+(require 'ffap)
+(require 'uniquify)
+(require 'ansi-color)
+(require 'recentf)
+(require 'linum)
+(require 'smooth-scrolling)
+(require 'whitespace)
+(require 'dired-x)
+(require 'compile)
+(ido-mode t)
+(menu-bar-mode -1)
+(normal-erase-is-backspace-mode 1)
+(put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
+(setq column-number-mode t)
+(setq inhibit-startup-message t)
+(setq save-abbrevs nil)
+(setq show-trailing-whitespace t)
+(setq suggest-key-bindings t)
+(setq vc-follow-symlinks t)
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit autoface-default :strike-through nil :underline nil :slant normal :weight normal :height 120 :width normal :family "monaco"))))
+ '(column-marker-1 ((t (:background "red"))))
+ '(diff-added ((t (:foreground "cyan"))))
+ '(flymake-errline ((((class color) (background light)) (:background "Red"))))
+ '(font-lock-comment-face ((((class color) (min-colors 8) (background light)) (:foreground "red"))))
+ '(fundamental-mode-default ((t (:inherit default))))
+ '(highlight ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
+ '(isearch ((((class color) (min-colors 8)) (:background "yellow" :foreground "black"))))
+ '(linum ((t (:foreground "black" :weight bold))))
+ '(region ((((class color) (min-colors 8)) (:background "white" :foreground "magenta"))))
+ '(secondary-selection ((((class color) (min-colors 8)) (:background "gray" :foreground "cyan"))))
+ '(show-paren-match ((((class color) (background light)) (:background "black"))))
+ '(vertical-border ((t nil))))
+
+;; ------------
+;; -- Macros --
+;; ------------
+(load "defuns-config.el")
+(fset 'align-equals "\C-[xalign-regex\C-m=\C-m")
+(global-set-key "\M-=" 'align-equals)
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c;" 'comment-or-uncomment-region)
+(global-set-key "\M-n" 'next5)
+(global-set-key "\M-p" 'prev5)
+(global-set-key "\M-o" 'other-window)
+(global-set-key "\M-i" 'back-window)
+(global-set-key "\C-z" 'zap-to-char)
+(global-set-key "\C-h" 'backward-delete-char)
+(global-set-key "\M-d" 'delete-word)
+(global-set-key "\M-h" 'backward-delete-word)
+(global-set-key "\M-u" 'zap-to-char)
+
+;; ---------------------------
+;; -- JS Mode configuration --
+;; ---------------------------
+(load "js-config.el")
+(add-to-list 'load-path "~/.emacs.d/jade-mode") ;; github.com/brianc/jade-mode
+(require 'sws-mode)
+(require 'jade-mode)    
+(add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
+(add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+
+;;Seli
+(setq-default indent-tabs-mode nil)
+
+(setq inferior-js-mode-hook
+      (lambda ()
+        ;; We like nice colors
+        (ansi-color-for-comint-mode-on)
+        ;; Deal with some prompt nonsense
+        (add-to-list
+         'comint-preoutput-filter-functions
+         (lambda (output)
+           (replace-regexp-in-string "\033\\[[0-9]+[GK]" "" output)))))
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+(global-set-key "\M- " 'hippie-expand)
+;; Seli auto complete
+
+(add-to-list 'load-path "/home/adrian/.emacs.d/auto-complete")                                                                          
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "/home/adrian/.emacs.d/auto-complete/ac-dict")                                                  
+(ac-config-default)
+
+
+
+(require 'popup)
+(add-to-list 'load-path
+              "~/.emacs.d/plugins/yasnippet-0.6.1c")
+(require 'yasnippet) ;; not yasnippet-bundle
+(yas/initialize)
+(yas/load-directory "~/.emacs.d/plugins/yasnippet-0.6.1c/snippets")
+
+;;(yas-global-mode 1) 
+;; add some shotcuts in popup menu mode
+(define-key popup-menu-keymap (kbd "M-n") 'popup-next)
+(define-key popup-menu-keymap (kbd "TAB") 'popup-next)
+(define-key popup-menu-keymap (kbd "<tab>") 'popup-next)
+(define-key popup-menu-keymap (kbd "<backtab>") 'popup-previous)
+(define-key popup-menu-keymap (kbd "M-p") 'popup-previous)
+ 
+(defun yas/popup-isearch-prompt (prompt choices &optional display-fn)
+  (when (featurep 'popup)
+    (popup-menu*
+     (mapcar
+      (lambda (choice)
+        (popup-make-item
+         (or (and display-fn (funcall display-fn choice))
+             choice)
+         :value choice))
+      choices)
+     :prompt prompt
+     ;; start ispearch mode immediately
+     :isearch t
+     )))
+ 
+(setq yas/prompt-functions '(yas/popup-isearch-prompt yas/no-prompt))
+
+;;(require 'yasnippet)
+;;(add-to-list 'ac-sources 'ac-source-yasnippet)
+
+
+ ; Function to run Tidy HTML parser on buffer
+ ; NOTE: this requires external Tidy program
+ (defun tidy-buffer ()
+  "Run Tidy HTML parser on current buffer."
+  (interactive)
+  (if (get-buffer "tidy-errs") (kill-buffer "tidy-errs"))
+  (shell-command-on-region (point-min) (point-max)
+    "tidy -f /tmp/tidy-errs -q -i -wrap 72 -c" t)
+  (find-file-other-window "/tmp/tidy-errs")
+  (other-window 1)
+  (delete-file "/tmp/tidy-errs")
+  (message "buffer tidy'ed")
+ )
+;; (global-set-key (kbd "C-x t") 'tidy-buffer)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(dired-dwim-target t))
